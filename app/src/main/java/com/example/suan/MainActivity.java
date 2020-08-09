dc5333f831141e9e9cbff902551ad8fda83af736
mac_home <953605663@qq.com>
2020-08-09 20:35:08 +0800

    2020/8/9

---
+++ app/src/main/java/com/example/suan/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.suan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
